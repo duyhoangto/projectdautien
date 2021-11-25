214c24f6674e891c9abb95a5bcb21417f9c9b7f3
DUYHOANGTO <duyhoangto2003@gmail.com>
2021-11-25 20:21:42 +0700

    first time project

---
+++ hoangto.js
@@ -0,0 +1,11 @@
+
+alert('WELCOME TO DUYHOANGTO SHOP')
+
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
